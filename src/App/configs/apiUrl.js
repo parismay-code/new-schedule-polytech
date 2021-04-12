c67a19b3549281d034ce426d149379d3e1148f5d
mentalaffect <mentalaffectdota@gmail.com>
2021-04-12 11:30:02 +0300

    v1.0.0

---
+++ src/App/configs/apiUrl.js
@@ -0,0 +1,3 @@
+export const apiUrl = {
+    schedule: 'https://rasp.dmami.ru/site/group'
+}
---
+++ src/App/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import styles from './HomePage.modules.scss';
+
+const HomePage = () => {
+    return <div className={styles['home-page']}>
+        home page
+    </div>
+}
+
+export default HomePage;
